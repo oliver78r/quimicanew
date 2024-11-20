da0eac929fa46c2d21ed6d8a9c7cbeee50151c20
oliver78r <villamonteoliver8@gmail.com>
2024-11-20 09:24:42 -0600

    yaaaaa

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-
+// Función para abrir un modal
 function openModal(modalId) {
     // Cerrar cualquier modal que esté abierto
     const modals = document.querySelectorAll('.modal');
     modals.forEach(modal => {
         modal.style.display = 'none';
     });
-    
+
     // Abrir el modal seleccionado
     document.getElementById(modalId).style.display = 'flex';
 }
 
+// Función para cerrar el modal
 function closeModal(modalId) {
     document.getElementById(modalId).style.display = 'none';
 }
@@ -20,6 +21,21 @@ window.onclick = function(event) {
         event.target.style.display = 'none';
     }
 }
+
+// Desactivar el scroll al cargar la página
 document.addEventListener('DOMContentLoaded', () => {
     document.body.style.overflow = 'hidden'; // Desactiva el scroll desde el inicio
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const inicioLink = document.querySelector('a[href="#inicio"]');
+
+    inicioLink.addEventListener("click", function(event) {
+        event.preventDefault(); // Evita que se ejecute el comportamiento predeterminado
+
+        // Desplazamiento suave a la sección de inicio
+        document.querySelector('#inicio').scrollIntoView({
+            behavior: 'smooth' // Hacer scroll suave
+        });
+    });
 });
